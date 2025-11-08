167b26ff2b4cd4dbcf52aa78f8b1bd3cfa64e432
ChaChaCha <rmps.ric.0147@nifty.com>
2025-11-08 15:27:17 +0900

    カードコンポーネントを作成

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
+function Card(props) {
+  return (
+    <div className="border rounded-md border-gray-600 px-4">
+      {props.text}
+    </div>
+  )
+}
+
 export default function Home() {
   const name = 'ChaCha'
   return (
     <>
-      <div className="bg-gray-700">
-        Hello, {name}
+      <div className="p-20 space-y-4">
+        <div>Hello, {name}</div>
+        <Card text="親から渡された文字列"/>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </>
   );
