41efa269f289320a575f48502b9927b9ccf9703f
ChaChaCha <rmps.ric.0147@nifty.com>
2025-11-16 00:24:01 +0900

    blog postのテスト

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,9 +1,16 @@
+import { MDXRemote } from "next-mdx-remote/rsc";
+import fs from 'fs'
+import path from 'path'
+
 async function BlogPage({ params }) {
-    const { slug } = await params;
+    const { slug } = await params
+    const markdown = fs.readFileSync(
+        path.join(process.cwd(), 'content', `${slug}.mdx`)
+    )
     return (
-        <>
-        Hello { slug }
-        </>
+        <article className="prose dark:prose-invert">
+            <MDXRemote source={markdown}/>
+        </article>
     )
 }
 
