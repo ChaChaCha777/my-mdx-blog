d4da518968562242d4745d87668ea3c27334d0fe
ChaChaCha <rmps.ric.0147@nifty.com>
2025-11-16 10:37:22 +0900

    記事一覧ページにリンクを追加

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 import { getPost } from '@/lib/posts'
+import Link from 'next/link'
 
 export default async function BlogPostPage() {
     const files = fs.readdirSync(
@@ -21,7 +22,22 @@ export default async function BlogPostPage() {
     console.log(posts)
     return (
         <>
-            <h1>最近の投稿</h1>
+            <h1 className='mb-8 text-xl'>最近の投稿</h1>
+
+            <div className='text-lg text-gray-600 dark:text-gray-400 mb-8'>
+                最近の投稿をチェックする。
+            </div>
+
+            <hr />
+
+            <ul className='grid grid-cols-1 md:grid-cols-2 gap-8'>
+                {posts.map(post =>(
+                    <li key={post.slug}>
+                        <Link href={`/blog/${post.slug}`}className='text-2xl font-semibold text-gray-800 dark:text-gray-200'>{post.frontmatter.title}</Link>
+                        <div className='text-gray-400 text-sm mt-2'>{post.frontmatter.date}</div>
+                    </li>
+                ))}
+            </ul>
         </>
     )
 }
--- components/navigation.js
+++ components/navigation.js
@@ -4,7 +4,6 @@ function Navigation() {
     return (
         <nav className="font-mono">
             <ul className="flex md:space-x-4 flex-col md:flex-row">
-                <li><Link href="/" className={styles.link}>Home</Link></li>
                 <li><Link href="/about" className={styles.link}>About</Link></li>
                 <li><Link href="/blog" className={styles.link}>Blogs</Link></li>
                 
