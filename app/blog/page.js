882e8a49b90302109db4b3e3b381da0638495301
ChaChaCha <rmps.ric.0147@nifty.com>
2025-11-16 10:47:10 +0900

    全ての投稿を取得する部分を関数として分離

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,25 +1,8 @@
-import fs from 'fs'
-import path from 'path'
-import { getPost } from '@/lib/posts'
+import { getPosts } from '@/lib/posts'
 import Link from 'next/link'
 
 export default async function BlogPostPage() {
-    const files = fs.readdirSync(
-        path.join(
-            process.cwd(), 'content'
-        )
-    )
-    
-    const posts = await Promise.all(files.map(async filename => {
-        const {frontmatter} = await getPost(filename)
-
-        return {
-            frontmatter,
-            slug: filename.replace('.mdx', '')
-        }
-    }))
-
-    console.log(posts)
+    const posts = await getPosts()
     return (
         <>
             <h1 className='mb-8 text-xl'>最近の投稿</h1>
