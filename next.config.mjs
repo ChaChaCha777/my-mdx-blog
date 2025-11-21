b7d22a3e4579066050d17e7f33f9241804fa28d1
ChaChaCha <rmps.ric.0147@nifty.com>
2025-11-21 23:12:55 +0900

    npm run dev時のmultiple lock file時のエラーを消すため、turbopackの設定を追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 import createMDX from '@next/mdx'
 
+const __dirname = process.cwd()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // .mdx拡張子のファイルを許可
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   // オプションで、他のNext.js設定をここに追加してください
+  turbopack: {
+    root: __dirname
+  }
 }
  
 const withMDX = createMDX({})
